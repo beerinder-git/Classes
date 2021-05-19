ab0772823c80b771863d59b0fc1a1c5e30aa1e76
mmeetk2314 <mk060@atcvisioncollege.ac.nz>
2021-05-20 11:50:04 +1200

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classes
+{
+
+    public class Car
+    {
+
+        // Member variables
+
+        // Color of the car
+        public string color { get; set; }
+
+        // Make of the car
+        private string make { get; set; }
+
+        // Year of the car
+        public string year { get; set; }
+
+        // Functions
+        // 1 . Constructors
+        public Car()
+        {
+            Console.WriteLine("I am in Constructor");
+            color = "RED";
+            make = "Toyota";
+            year = "2021";
+        }
+        // 2 . Methods/Functions
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Car car_obj = new Car();
+
+            
+        }
+    }
+}
